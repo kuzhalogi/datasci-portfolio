0652f2f969c001c84d9fc6fe3385311a85584623
kuzhalogi <kuzhalogiyogis@gmail.com>
2025-12-25 20:28:27 +0100

    changed url to host

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-  base: 'https://github.com/yourusername/datasci-portfolio.git',
+  base: 'https://github.com/kuzhalogi/datasci-portfolio.git',
   plugins: [react()],
 });
